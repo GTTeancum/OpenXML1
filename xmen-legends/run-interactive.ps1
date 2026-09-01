824092010f5feeb501d03d9d51707e42ca43add3
GTTeancum <smmelton@gmail.com>
2026-09-01 16:40:46 -0400

    Retain one rolling gameplay capture

--- xmen-legends/run-interactive.ps1
+++ xmen-legends/run-interactive.ps1
@@ -69,6 +69,7 @@ try {
     $startInfo.Environment['PS2X_XMEN_HOST_CLOCK'] = '1'
     $startInfo.Environment['PS2X_FAST_FORWARD_XMEN_LEGAL'] = '1'
     $startInfo.Environment['PS2X_XMEN_PROGRESS_TRACE'] = '1'
+    $startInfo.Environment['PS2X_CAPTURE_LATEST_PRESENT_INTERVAL'] = '128'
 
     $process = [System.Diagnostics.Process]::new()
     $process.StartInfo = $startInfo
